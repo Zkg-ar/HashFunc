5171b6b6394ac60e0b53a4ca38eb7c2a4da3180d
Kzg-ar <kalanderyan99@gmail.com>
2020-09-22 10:57:21 +0300

    1

---
+++ HashFunc1/src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Main {
+
+    private static String hash = "";
+    private static char [] alphabet = new char[]{
+            'а','б','в','г','д','е','ё','ж',
+            'з','и','й','к','л','м','н','о',
+            'п','р','с','т','у','ф','х','ц',
+            'ч','ш','щ','ъ','ы','ь','э','ю','я'
+    };
+
+    public static void main(String[] args) {
+        System.out.println(HashFunc("угу,ага,оно,сто,ост,сон,нос"));
+    }
+
+    public static String HashFunc(String input){
+        int sum = 0,first=0,last=0;
+        input = input.toLowerCase();
+        String [] words = input.split("\\s|[.!,?\\-]");
+
+        for (int i = 0;i<words.length;i++){
+            for(int j = 0;j<words[i].length();j++){
+
+                for (int k = 0;k<alphabet.length;k++) {
+                    if(words[i].charAt(0)==alphabet[k]){
+                        first=k+1;
+                    }
+                    if(words[i].charAt(words[i].length()-1)==alphabet[k]){
+                        last=k+2;
+                    }
+
+                    if (words[i].charAt(j) == alphabet[k]) {
+                        sum += k;
+                    }
+
+                }
+
+            }
+            sum = sum * ((last-first));
+            hash +=Integer.toString(sum)+" " ;
+            sum = 0;
+
+        }
+
+        return hash;
+    }
+
+}
+
